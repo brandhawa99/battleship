254674f89f9deea1a76b980d78bfabbf8b45e17a
Baltej Randhawa <baltej.randhawa0@gmail.com>
2022-01-13 16:30:44 -0800

    added linting

--- ships.js
+++ ships.js
@@ -1,10 +1,8 @@
-
-
 // the pos is an array a 2d array with as many values as the lenght of the ship 
 // ship of length 2 [[1,2][1,3]] so the ship takes up these spots in gameboard array 
-/* 
+/*
 spaces =
-sunk  : false, 
+sunk  : fals'e','e','e','e','e','e','e','e','e'
 pos : [
     {
         x:1,
@@ -24,15 +22,12 @@ pos : [
 ]
 
 */
-const shipFactory = (length ,spaces ) => {
-    let sunk = false
+const shipFactory = (spaces) => {
+  let sunk = false;
 
-
-    const hit = (pos) =>{
-        let x = pos.x; 
-        let y = pos.y; 
-        for(let i = 0; i<length; i++ ){
-            if(spaces[i].x == pos.x && spaces[i].y == pos.y){
+  const hit = (pos) => {
+    for (let i = 0; i < spaces.length; i += 1) {
+            if(spaces[i].x == pos.x && spaces[i].y === pos.y){
                 spaces[i].hit = true;
                 return true; 
             }
@@ -43,12 +38,12 @@ const shipFactory = (length ,spaces ) => {
 
     const hasSunk = () =>{
         let hits =0; 
-        for(let i = 0 ; i<length; i++){
+        for(let i = 0 ; i<spaces.length; i++){
             if(spaces[i].hit == true){
                 hits++; 
             }
         }
-        if(hits == length){
+        if(hits == spaces.length){
             sunk = true;
             return sunk
         }
@@ -59,7 +54,8 @@ const shipFactory = (length ,spaces ) => {
     }
     return {
         hit,
-        hasSunk
+        hasSunk,
+        spaces
     }
 
 }
@@ -67,25 +63,89 @@ const shipFactory = (length ,spaces ) => {
 
 const gameBoard = () =>{
 
-    let gameBoard = [
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-]
+    let board =  [
+                ['e','e','e','e','e','e','e','e','e','e'],
+                ['e','e','e','e','e','e','e','e','e','e'],
+                ['e','e','e','e','e','e','e','e','e','e'],
+                ['e','e','e','e','e','e','e','e','e','e'],
+                ['e','e','e','e','e','e','e','e','e','e'],
+                ['e','e','e','e','e','e','e','e','e','e'],
+                ['e','e','e','e','e','e','e','e','e','e'],
+                ['e','e','e','e','e','e','e','e','e','e'],
+                ['e','e','e','e','e','e','e','e','e','e'],
+                ['e','e','e','e','e','e','e','e','e','e'],] 
 
-const placeShip = () =>{
-    
+    const ships = []; 
+
+    const placeShip = (cords) =>{
+        
+        cords.forEach(cord =>{
+            let x = cord.x;
+            let y = cord.y; 
+            board[x][y] = 's';
+        })
+
+        const ship = shipFactory(cords)
+        ships.push(ship);
+        
+    }
+    const recieveAttack = (cords) =>{
+        let flag = false
+        
+        ships.forEach(ship =>{
+            if(ship.hit(cords)){
+                flag = true; 
+            }
+        })
+        if(flag){
+            board[cords.x][cords.y] = 'h'
+        }else{
+            board[cords.x][cords.y] = 'm'
+
+        }
+
+    }
+
+    // goes through ships array and checks all ships to see if there are any left standing
+    // returns true if all ships have sunk 
+    //else returns false; 
+    const allSunk = () =>{
+        let shipsCount = ships.length
+        let i =0 ; 
+        ships.forEach(ship =>{
+            if(ship.hasSunk()){
+                i++;
+            }
+        })
+
+        if( i === shipsCount){
+            return true;
+        }
+        return false; 
+
+    }
+    return{
+        ships,
+        placeShip,
+        recieveAttack,
+        allSunk,
+        board
+
+    }
+
 }
 
 
 
 
-}
 
+
+
+
+
+
+
+export{
+    shipFactory,
+    gameBoard
+}
--- test/ships.test.js
+++ test/ships.test.js
@@ -1,2 +1,60 @@
-import shipFactory from ('../ships')
+import {shipFactory, gameBoard} from '../ships'
 
+test('check if ship has sunk',() =>{
+  const ship = shipFactory([{x:0,y:0,hit:false},{x:0,y:1,hit:false}])
+  ship.hit({x:0,y:1});
+  ship.hit({x:0,y:0});
+
+  expect(ship.hasSunk()).toBeTruthy()
+
+})
+
+test('check if ship has Not sunk',() =>{
+    const ship = shipFactory([{x:0,y:0,hit:false},{x:0,y:1,hit:false}])
+    ship.hit({x:0,y:1});
+    ship.hit({x:0,y:3});
+
+    expect(ship.hasSunk()).toBeFalsy()
+
+})
+
+test('if ship has been placed at correct corrdinates', ()=>{
+
+    const game = gameBoard(); 
+    game.placeShip([{x:0,y:3,hit:false},{x:0,y:1,hit:false},{x:0,y:2,hit:false}])
+
+    expect(game.ships[0].spaces[0]).toEqual({x:0,y:3,hit:false})
+    expect(game.ships[0].spaces[1]).toEqual({x:0,y:1,hit:false})
+    expect(game.ships[0].spaces[2]).toEqual({x:0,y:2,hit:false})
+
+})
+
+test('test if ships can be placed and update board', () =>{
+    const myBoard = gameBoard(); 
+
+    myBoard.placeShip([{x:0,y:3,hit:false},{x:0,y:1,hit:false},{x:0,y:2,hit:false}])
+    expect(myBoard.board[0][3]).toBe('s');
+    expect(myBoard.board[0][2]).toBe('s');
+    expect(myBoard.board[0][1]).toBe('s');
+
+})
+test('test if ships can be atacked',() =>{
+    const myBoard = gameBoard(); 
+    myBoard.placeShip([{x:0,y:3,hit:false},{x:0,y:1,hit:false},{x:0,y:2,hit:false}])
+    myBoard.recieveAttack({x:0,y:3});
+    expect(myBoard.board[0][3]).toBe('h');
+
+})
+
+
+test('test all sunk function', () =>{
+    const myBoard = gameBoard();
+    myBoard.placeShip([{x:0,y:3,hit:false},{x:0,y:1,hit:false},{x:0,y:2,hit:false}])
+    myBoard.recieveAttack({x:0,y:3});
+    myBoard.recieveAttack({x:0,y:2});
+    myBoard.recieveAttack({x:0,y:1});
+
+    expect(myBoard.allSunk()).toBe(true);
+
+
+})
